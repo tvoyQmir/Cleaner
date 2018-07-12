b6ead92dc68e7f822d059402b70ed8f9c9443016
qwe <artyk16@gmail.com>
2018-07-12 20:11:47 +0300

    Change Row to Col

--- main.cpp
+++ main.cpp
@@ -1,202 +1,112 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-
-void ShowBorderArray(const int ROW, const int COL,char arr[][12]);
-void FillBorderArray(const int ROWS, const int COLS,char array[][12]);
-int GameOver();
-void randBlocker(int &rand_a,int &rand_b,char array[][12]);
-void Points(int &points);
-
-
-
-int main() {
-    int points = 0;
-    srand(time(NULL));
-    int rand_a = 0;
-    int rand_b = 0;
-    const int ROWS = 12;
-    const int COLS = 12;
-
-    char array[ROWS][COLS];
-
-    FillBorderArray(ROWS, COLS, array);
-
-    int a = 5;
-    int b = 5;
-
-    array[a][b] = '*';
-
-    ShowBorderArray(ROWS, COLS, array);
-
-
-    int count;
-    int h = 0;
-    do
+#define Info
+class  Array
         {
-            if(a == 0 || b == 0 || a == 12 || b == 12 )
+public:
+            int ROW ;
+            int COL ;
+            int **arr;
+
+    Array(int valueR, int valueC)
             {
-                GameOver();
-                exit(0);
-            }
+                this-> ROW = valueR;
+                this-> COL = valueC;
+                this-> arr = new int*[ROW];
+                for (int i = 0; i <ROW ; ++i) {
 
-           randBlocker(rand_a,rand_b,array);
-
-            count = getchar();
-
-            switch (count) {
-                case  87 :
-                    //UP
-
-                    {
-
-                        if(array[a-1][b] == '#')
-                        {
-                           std::cout<<"Points plus"<<std::endl;
-                            points++;
-                        }
-                        array[a][b] = ' ';
-                        a--;
-                        array[a][b] = '*';
-                        Points(points);
-                       ShowBorderArray(ROWS, COLS, array);
-                        break;
-
-                    }
-
-                    case 65:
-
-                        //left
-                        {
-
-                            if(array[a][b-1] == '#')
-                            { std::cout<<"Points plus"<<std::endl;
-
-                                points++;
-                            }
-                            array[a][b] = ' ';
-                            b--;
-                            array[a][b] = '*';
-                            Points(points);
-                            ShowBorderArray(ROWS, COLS, array);
-                            break;
-
-
-
-                        }
-
-
-                        case 68:
-
-                            //right
-
-                            {
-                                if(array[a][b+1] == '#')
-                                { std::cout<<"Points plus"<<std::endl;
-
-                                    points++;
-                                }
-                                array[a][b] = ' ';
-                                b++;
-                                array[a][b] = '*';
-                                Points(points);
-                                ShowBorderArray(ROWS, COLS, array);
-                                break;
-
-
-                            }
-
-
-                            case 83:
-                                //DOWN
-
-                                {
-
-                                    if(array[a+1][b] == '#')
-                                    { std::cout<<"Points plus"<<std::endl;
-                                        points++;
-                                    }
-                                    array[a][b] = ' ';
-                                    a++;
-                                    array[a][b] = '*';
-                                    Points(points);
-                                    ShowBorderArray(ROWS, COLS, array);
-                                    break;
-
-
-                                }
-
-
-                                default:
-
-                                    {
-
-                                        std::cout<<"Press E for quit."<<std::endl;
-                                        break;
-
-                                    }
-                                    }
-
-        }while (count== 'E');
-    return 0;
-}
-
-void ShowBorderArray(const int ROWS, const int COLS,char arr[][12])
-{
-    for (int i = 0; i < ROWS; ++i) {
-        for (int j = 0; j < COLS ; ++j) {
-            std::cout << arr[i][j]<<"\t";
-        }
-        std::cout << std::endl;
-    }
-}
-
-void FillBorderArray(const int ROWS, const int COLS,char array[][12])
-{
-    for (int i = 0; i < ROWS; ++i) {
-        for (int j = 0; j < COLS ; ++j) {
-
-            if(i==0||i==(ROWS-1))
-            {
-                array[i][j] = '#';
-            }
-            else
-                {
-                    if(j==0||j==(COLS-1))
-                    {
-                        array[i][j] = '#';
-                    }
-                    else array[i][j] = ' ';
+                    this-> arr[i] = new int[COL];
                 }
 
+                #ifdef Info
+
+                std::cout<<"KONSTR  "<<this<<std::endl;
+
+                #endif
+
+
+            }
+
+
+
+    void FillArray()
+    {
+        for (int i = 0; i < ROW ; ++i) {
+            for (int j = 0; j < COL; ++j) {
+
+
+                arr[i][j] = rand() % 10;
+            }
+        }
+    }
+
+ void Change(Array & other)
+    { int a = other.COL;
+        this->COL = other.ROW;
+        this->ROW = a;
+
+
+       int ** arr1 = new int*[ROW];
+        for (int i = 0; i <ROW ; ++i) {
+
+            arr1[i] = new int[COL];
+        }
+
+        for (int i = 0, b = 0; i <ROW ; i++, b++) {
+            for (int j = 0, a = 0; j <COL ; j++, a++) {
+
+
+              arr1[i][j] = other.arr[a][b];
+
+            }
 
         }
-        std::cout << std::endl;
 
+delete[] arr;
+
+        this->arr = arr1;
     }
-}
 
-int GameOver()
-{
-    std::cout<<"Game over. You lose"<<std::endl;
-    return 1;
-}
+  void ShowArray()
+  {
+      for (int i = 0; i < ROW ; ++i) {
+          for (int j = 0; j < COL; ++j) {
 
-void randBlocker(int &rand_a,int &rand_b,char array[][12])
-{
-    M1:
-    rand_a=rand()%10;
-    rand_b=rand()%10;
-    if(  array[rand_a][rand_b] == '#')
+              std::cout<<arr[i][j] <<"\t";
+          }
+
+          std::cout<<std::endl;
+      }
+
+  }
+    void ShowInfo()
     {
-        goto M1;
-
+            std::cout <<"This is another array"<< std::endl;
     }
-    array[rand_a][rand_b]='#';
+    ~Array(){
 
-}
+        #ifdef Info
 
-void Points(int &points)
-{
-    std::cout<<"You have "<<points<<" point(s)."<<std::endl;
+        std::cout << "\nDISTR " << this << std::endl;
+
+        #endif
+                delete[] arr;
+            }
+
+};
+int main() {
+
+    Array arr(2,5);
+
+    arr.FillArray();
+
+    arr.ShowArray();
+
+    arr.ShowInfo();
+
+    arr.Change(arr);
+
+    arr.ShowArray();
+
+
+    return 0;
 }
