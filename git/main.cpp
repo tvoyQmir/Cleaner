f92314939dc8725554448f44e31daf02adc1d5ea
qwe <artyk16@gmail.com>
2018-09-04 16:04:48 +0300

    vector to list

---
+++ git/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <list>
+
+template <typename T>
+std::list<T> VectorToList (std::vector<T> v)
+{
+    std::list<int> l;
+
+    for (auto el:v)
+    {
+        l.push_back(el);
+    }
+    return l;
+}
+
+template <typename T1>
+void PrintList (std::list<T1> list)
+{
+    for(auto element:list)
+    {
+        std::cout<<element<<std::endl;
+    }
+}
+int main() {
+
+    std::vector<int> vector = {1,2,3,4};
+
+    std::list<int> l = VectorToList(vector);
+
+    PrintList(l);
+
+    return 0;
+}
